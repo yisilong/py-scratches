d324bee1be9628540e37411a75a286fd335ca20f
likebeta <ixxoo.me@gmail.com>
2021-01-11 15:33:11 +0800

    animation

--- ai/ml_regression_1.py
+++ ai/ml_regression_1.py
@@ -2,63 +2,104 @@
 # coding: utf-8
 
 import numpy as np
+from matplotlib import animation
 import matplotlib.pyplot as plt
 
-# f(theta) = theta0 + theta1 * x
 
-# 数据图像
-train = np.loadtxt('click.csv', delimiter=',', skiprows=1)
-train_x = train[:, 0]
-train_y = train[:, 1]
+class MLRegression(object):
+    """
+    一次函数
+    f(theta) = theta0 + theta1 * x
+    """
 
-# 数据标准化/z-score规范化
-mu = np.mean(train_x)
-sigma = np.std(train_x)
+    def __init__(self, file_name, eta=0.001, delimiter=',', skiprows=1):
+        self.eta = eta
+        self._theta0 = np.random.rand()
+        self._theta1 = np.random.rand()
+        train = np.loadtxt(file_name, delimiter=delimiter, skiprows=skiprows)
+        self.train_x = train[:, 0]
+        self.train_y = train[:, 1]
+        self.mu = np.mean(self.train_x)
+        self.sigma = np.std(self.train_x)
+
+    def standardize(self, x):
+        return (x - self.mu) / self.sigma
+
+    # 预测函数
+    def f(self, x):
+        return self._theta0 + self._theta1 * x
+
+    # 目标函数
+    def E(self, x, y):
+        return (1.0 / 2) * np.sum((y - self.f(x)) ** 2)
+
+    def draw(self):
+        train_z = self.standardize(self.train_x)
+        plt.plot(train_z, self.train_y, 'o')
+
+        diff, count = 1, 1
+        error = self.E(train_z, self.train_y)
+        while diff > 0.01:
+            new_theta0 = self._theta0 - self.eta * np.sum(self.f(train_z) - self.train_y)
+            new_theta1 = self._theta1 - self.eta * np.sum((self.f(train_z) - self.train_y) * train_z)
+            self._theta0, self._theta1 = new_theta0, new_theta1
+            curr_error = self.E(train_z, self.train_y)
+            diff = error - curr_error
+            error = curr_error
+            count += 1
+            print(f'第{count}次， theta0:{self._theta0:.3f}, theta1:{self._theta1:.3f}, 差值:{diff:.4f}')
+
+        xs = np.linspace(-3, 3, 100)
+        ys = self.f(xs)
+        plt.plot(xs, ys)
+
+        plt.show()
+
+    def step(self, train_z):
+        diff, count = 1, 1
+        error = self.E(train_z, self.train_y)
+        while diff > 0.01:
+            new_theta0 = self._theta0 - self.eta * np.sum(self.f(train_z) - self.train_y)
+            new_theta1 = self._theta1 - self.eta * np.sum((self.f(train_z) - self.train_y) * train_z)
+            self._theta0, self._theta1 = new_theta0, new_theta1
+            curr_error = self.E(train_z, self.train_y)
+            diff = error - curr_error
+            error = curr_error
+            count += 1
+            print(f'第{count}次， theta0:{self._theta0:.3f}, theta1:{self._theta1:.3f}, 差值:{diff:.4f}')
+            yield object()
+
+    def draw_animation(self):
+        fig, ax = plt.subplots()
+
+        train_z = self.standardize(self.train_x)
+        ax.plot(train_z, self.train_y, 'o')
+
+        xs = np.linspace(-3, 3, 100)
+        ys = self.f(xs)
+        line, = ax.plot(xs, ys)
+
+        step = self.step(train_z)
+
+        def animate(i):
+            ys = self.f(xs)
+            line.set_ydata(ys)
+            return line,
+
+        def init():
+            line.set_ydata(ys)
+            return line,
+
+        ani = animation.FuncAnimation(fig=fig,
+                                      func=animate,
+                                      frames=self.step(train_z),
+                                      init_func=init,
+                                      interval=20,
+                                      blit=False,
+                                      repeat=False)
+        plt.show()
 
 
-def standardize(x):
-    return (x - mu) / sigma
-
-
-train_z = standardize(train_x)
-
-plt.plot(train_z, train_y, 'o')
-
-# 训练
-theta0 = np.random.rand()
-theta1 = np.random.rand()
-
-
-# 预测函数
-def f(x):
-    return theta0 + theta1 * x
-
-
-# 目标函数
-def E(x, y):
-    return (1.0 / 2) * np.sum((y - f(x)) ** 2)
-
-
-# 学习率
-ETA = 0.001
-# 误差的差值
-diff = 1
-# 更新次数
-count = 1
-# 重复学习
-error = E(train_z, train_y)
-while diff > 0.01:
-    new_theta0 = theta0 - ETA * np.sum(f(train_z) - train_y)
-    new_theta1 = theta1 - ETA * np.sum((f(train_z) - train_y) * train_z)
-    theta0, theta1 = new_theta0, new_theta1
-    curr_error = E(train_z, train_y)
-    diff = error - curr_error
-    error = curr_error
-    count += 1
-    print(f'第{count}次， theta0:{theta0:.3f}, theta1:{theta1:.3f}, 差值:{diff:.4f}')
-
-xs = np.linspace(-3, 3, 100)
-ys = f(xs)
-plt.plot(xs, ys)
-
-plt.show()
+if __name__ == '__main__':
+    ml = MLRegression('click.csv')
+    ml.draw_animation()
