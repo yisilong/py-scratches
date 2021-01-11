9b6b3050804af4c6c064b0d2d2ba347aaba8f4fb
likebeta <ixxoo.me@gmail.com>
2021-01-11 21:05:52 +0800

    animation

--- ai/ml_regression_3.py
+++ ai/ml_regression_3.py
@@ -2,70 +2,109 @@
 # coding: utf-8
 
 import numpy as np
+from matplotlib import animation
 import matplotlib.pyplot as plt
 
-# f(theta) = theta0 + theta1 * x + theta2 * x^2
 
-# 数据图像
-train = np.loadtxt('click.csv', delimiter=',', skiprows=1)
-train_x = train[:, 0]
-train_y = train[:, 1]
+class MLRegression(object):
+    """
+    二次函数
+    f(theta) = theta0 + theta1 * x + theta2 * x * x
+    """
 
-# 数据标准化/z-score规范化
-mu = np.mean(train_x)
-sigma = np.std(train_x)
+    def __init__(self, file_name, eta=0.001, delimiter=',', skiprows=1):
+        self.eta = eta
+        self._theta = np.random.rand(3)
+        train = np.loadtxt(file_name, delimiter=delimiter, skiprows=skiprows)
+        self.train_x = train[:, 0]
+        self.train_y = train[:, 1]
+        self.mu = np.mean(self.train_x)
+        self.sigma = np.std(self.train_x)
+
+    def standardize(self, x):
+        return (x - self.mu) / self.sigma
+
+    def to_matrix(self, x):
+        return np.vstack([np.ones(x.shape[0]), x, x ** 2]).T
+
+    # 预测函数
+    def f(self, x):
+        return np.dot(x, self._theta)
+
+    # 目标函数
+    def MSE(self, x, y):
+        return (1.0 / x.shape[0]) * np.sum((y - self.f(x)) ** 2)
+
+    def draw(self):
+        train_z = self.standardize(self.train_x)
+        plt.plot(train_z, self.train_y, 'o')
+        X = self.to_matrix(train_z)
+
+        diff, count = 1, 1
+        errors = [self.MSE(X, self.train_y)]
+        while diff > 0.01:
+            # 为了调整训练数据的顺序，准备随机的序列
+            p = np.random.permutation(X.shape[0])
+            # 随机取出训练数据，使用随机梯度下降法更新参数
+            for x, y in zip(X[p, :], self.train_y[p]):
+                self._theta = self._theta - self.eta * (self.f(x) - y) * x
+            errors.append(self.MSE(X, self.train_y))
+            diff = errors[-2] - errors[-1]
+            print(f'第{count}次， theta:{self._theta}, 差值:{diff:.4f}')
+            count += 1
+
+        xs = np.linspace(-3, 3, 100)
+        ys = self.f(self.to_matrix(xs))
+        plt.plot(xs, ys)
+
+        plt.show()
+
+    def step(self, train_z):
+        X = self.to_matrix(train_z)
+
+        diff, count = 1, 1
+        errors = [self.MSE(X, self.train_y)]
+        while diff > 0.01:
+            # 为了调整训练数据的顺序，准备随机的序列
+            p = np.random.permutation(X.shape[0])
+            # 随机取出训练数据，使用随机梯度下降法更新参数
+            for x, y in zip(X[p, :], self.train_y[p]):
+                self._theta = self._theta - self.eta * (self.f(x) - y) * x
+            errors.append(self.MSE(X, self.train_y))
+            diff = errors[-2] - errors[-1]
+            print(f'第{count}次， theta:{self._theta}, 差值:{diff:.4f}')
+            count += 1
+            yield object()
+
+    def draw_animation(self):
+        fig, ax = plt.subplots()
+
+        train_z = self.standardize(self.train_x)
+        ax.plot(train_z, self.train_y, 'o')
+
+        xs = np.linspace(-3, 3, 100)
+        ys = self.f(self.to_matrix(xs))
+        line, = ax.plot(xs, ys)
+
+        def animate(i):
+            ys = self.f(self.to_matrix(xs))
+            line.set_ydata(ys)
+            return line,
+
+        def init():
+            line.set_ydata(ys)
+            return line,
+
+        ani = animation.FuncAnimation(fig=fig,
+                                      func=animate,
+                                      frames=self.step(train_z),
+                                      init_func=init,
+                                      interval=5,
+                                      blit=False,
+                                      repeat=False)
+        plt.show()
 
 
-def standardize(x):
-    return (x - mu) / sigma
-
-
-train_z = standardize(train_x)
-
-plt.plot(train_z, train_y, 'o')
-
-# 训练
-theta = np.random.rand(3)
-
-
-def to_matrix(x):
-    return np.vstack([np.ones(x.shape[0]), x, x ** 2]).T
-
-
-X = to_matrix(train_z)
-
-
-# 预测函数
-def f(x):
-    return np.dot(x, theta)
-
-
-# 均方误差
-def MSE(x, y):
-    return (1.0 / x.shape[0]) * np.sum((y - f(x)) ** 2)
-
-
-# 学习率
-ETA = 0.001
-# 误差的差值
-diff = 1
-# 更新次数
-count = 1
-# 重复学习
-errors = [MSE(X, train_y)]
-while diff > 0.01:
-    # 为了调整训练数据的顺序，准备随机的序列
-    p = np.random.permutation(X.shape[0])
-    # 随机取出训练数据，使用随机梯度下降法更新参数
-    for x, y in zip(X[p, :], train_y[p]):
-        theta = theta - ETA * (f(x) - y) * x
-    errors.append(MSE(X, train_y))
-    diff = errors[-2] - errors[-1]
-    print(f'第{count}次， theta:{theta}, 差值:{diff:.4f}')
-    count += 1
-
-xs = np.linspace(-3, 3, 100)
-ys = to_matrix(xs)
-plt.plot(xs, f(to_matrix(xs)))
-
-plt.show()
+if __name__ == '__main__':
+    ml = MLRegression('click.csv')
+    ml.draw_animation()
